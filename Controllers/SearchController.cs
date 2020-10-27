322f026f5033ac8752676ea096f60543fbc7a5c4
Joel <uo226321@uniovi.es>
2020-10-27 21:22:54 +0100

    Closes #3, Closes #5 added filtering by username and get all users for search screen

---
+++ Controllers/SearchController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace RecYouBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+        private readonly IStreamAPI _streamApi;
+        private readonly IDatabaseConnection _database;
+
+        public SearchController(IStreamAPI streamAPI, IDatabaseConnection database)
+        {
+            _streamApi = streamAPI;
+            _database = database;
+
+        }
+
+        // GET: api/<SearchController>
+        [HttpGet]
+        public IEnumerable<Model.User> Get()
+        {
+            using (_database.GetInstance)
+            {
+                return _database.GetInstance.Query<Model.User>("SELECT username, pic_url FROM users");
+            }
+        }
+
+        // GET api/<SearchController>/al
+        [HttpGet("{searchname}")]
+        public IEnumerable<Model.User> Get(string searchname)
+        {
+            return _database.GetInstance.Query<Model.User>("SELECT username, pic_url FROM users WHERE username LIKE @user", new { user = "%" + searchname + "%" });
+        }
+
+    }
+}
