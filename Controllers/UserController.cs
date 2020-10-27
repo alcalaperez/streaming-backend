b2aa2485a712d67e1818d16aa68c6f90da7ee817
Joel <uo226321@uniovi.es>
2020-10-27 21:16:14 +0100

    Closes #1, Closes #4 adding a controller to register and get an specific user

---
+++ Controllers/UserController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using RecYouBackend.Model;
+using Stream;
+
+namespace RecYouBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IStreamAPI _streamApi;
+        private readonly IDatabaseConnection _database;
+
+        public UserController(IStreamAPI streamAPI, IDatabaseConnection database)
+        {
+            _streamApi = streamAPI;
+            _database = database;
+
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public void Get()
+        {
+            
+        }
+
+        // GET api/<UserController>/username/true/false
+        [HttpGet("{userName}/{light}")]
+        public async Task<Model.User> GetAsync(string userName, bool light)
+        {
+            if (light)
+            {               
+                return _database.GetInstance.QuerySingleOrDefault<Model.User>("SELECT username, pic_url FROM users where username=@user", new { user = userName });
+            } else
+            {
+                FullUser fullUser;
+                fullUser = _database.GetInstance.QuerySingleOrDefault<Model.FullUser>("SELECT username, pic_url FROM users where username=@user", new { user = userName });
+                IStreamFeed userFeed = _streamApi.StreamClient.Feed("user", userName);
+                fullUser.Posts = await userFeed.GetActivities();
+                fullUser.Followers = await userFeed.Followers();
+                fullUser.Following = await userFeed.Following();
+
+                return fullUser;
+            }
+
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public async Task<string> PostAsync([FromBody] UserDto userDto)
+        {
+            var userData = new Dictionary<string, object>()
+            {
+                {"profile_pic", userDto.ProfilePictureUrl}
+            };
+
+            try
+            {
+                // Add user to the Stream API
+                await _streamApi.StreamClient.Users.Add(userDto.Username, userData);
+                // Add user to the DB
+                _database.GetInstance.Execute("INSERT INTO users (username, pass, pic_url) VALUES(@user, @pass, @pic)", new { user = userDto.Username, pass = userDto.Password, pic = userDto.ProfilePictureUrl });
+                // Register user timeline to his profile (the user timeline will show his own posts)
+                IStreamFeed userTimeline = _streamApi.StreamClient.Feed("timeline", userDto.Username);
+                await userTimeline.FollowFeed("user", userDto.Username);
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+            return "User " + userDto.Username + " was created correctly.";
+
+        }
+
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecYouBackend.Model
+{
+    public class User
+    {
+        public User(string userName, string profilePic)
+        {
+            Username = userName;
+            Pic_url = profilePic;
+        }
+
+        public User()
+        { }
+
+        public string Username { get; set; }
+        public string Pic_url { get; set; }
+
+    }
+
+    public class FullUser : User
+    {
+        public IEnumerable<Stream.Follower> Following { get; internal set; }
+        public IEnumerable<Stream.Activity> Posts { get; internal set; }
+        public IEnumerable<Stream.Follower> Followers { get; internal set; }
+    }
+
+}
