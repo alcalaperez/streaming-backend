9d00fb84e352864f5a006e6f789d836d25420b6b
Joel <uo226321@uniovi.es>
2020-10-28 00:05:30 +0100

    Closes #9 added endpoint to add and delete posts

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RecYouBackend.Model;
+using Stream;
+
+
+namespace RecYouBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private readonly IStreamAPI _streamApi;
+
+        public PostsController(IStreamAPI streamAPI)
+        {
+            _streamApi = streamAPI;
+        }
+
+
+        // POST api/<PostsController>
+        [HttpPost]
+        [Authorize]
+        public void Post([FromBody] PostDto pdto)
+        {
+            IStreamFeed userTimeline = _streamApi.StreamClient.Feed("user", pdto.UserName);
+            var activityData = new Activity(pdto.UserName, "posts", pdto.AudioUrl + "," + pdto.PictureUrl)
+            {
+                ForeignId = pdto.AudioUrl,
+                Target = pdto.Description,
+            };
+
+
+            userTimeline.AddActivity(activityData);
+        }
+
+        // DELETE api/<PostsController>/5
+        [HttpDelete]
+        [Authorize]
+        public void Delete([FromBody] PostDto pdto)
+        {
+            IStreamFeed userTimeline = _streamApi.StreamClient.Feed("user", pdto.UserName);
+            userTimeline.RemoveActivity(pdto.AudioUrl, true);
+        }
+    }
+}
---
+++ Model/PostDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RecYouBackend.Model
+{
+    public class PostDto
+    {
+        public string PictureUrl { get; set; }
+        public string AudioUrl { get; set; }
+        public string Description { get; set; }
+        public string UserName { get; set; }
+
+    }
+}
