736cf62917e77c05a75068304328262d15d313f5
Joel <uo226321@uniovi.es>
2020-10-28 00:02:54 +0100

    Closes #7 added endpoints to follow and unfollow users

---
+++ Controllers/RelationsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RecYouBackend.Model;
+using Stream;
+
+
+namespace RecYouBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RelationsController : ControllerBase
+    {
+        private readonly IStreamAPI _streamApi;
+
+        public RelationsController(IStreamAPI streamAPI)
+        {
+            _streamApi = streamAPI;
+        }
+
+        // POST api/<RelationsController>
+        [HttpPost]
+        [Authorize]
+        public async Task PostAsync([FromBody] RelationDto rdto)
+        {
+            IStreamFeed userTimeline = _streamApi.StreamClient.Feed("timeline", rdto.InterestedUser);
+            await userTimeline.FollowFeed("user", rdto.UserToFollowUnfollow);
+        }
+
+        // DELETE api/<RelationsController>
+        [HttpDelete]
+        [Authorize]
+        public async Task DeleteAsync([FromBody] RelationDto rdto)
+        {
+            IStreamFeed userTimeline = _streamApi.StreamClient.Feed("timeline", rdto.InterestedUser);
+            await userTimeline.UnfollowFeed("user", rdto.UserToFollowUnfollow);
+        }
+    }
+}
---
+++ Model/RelationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RecYouBackend.Model
+{
+    public class RelationDto
+    {
+        public string InterestedUser { get; set; }
+        public string UserToFollowUnfollow { get; set; }
+
+    }
+}
