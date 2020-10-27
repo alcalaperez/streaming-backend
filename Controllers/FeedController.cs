cec19d9cdb6c3e6ada20a39b97b3a6168e8e1e11
Joel <uo226321@uniovi.es>
2020-10-28 00:03:56 +0100

    Closes #6 new controller for getting the timeline of an user

---
+++ Controllers/FeedController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Stream;
+
+namespace RecYouBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedController : ControllerBase
+    {
+        private readonly IStreamAPI _streamApi;
+
+        public FeedController(IStreamAPI streamAPI)
+        {
+            _streamApi = streamAPI;
+        }
+
+        // GET api/<FeedController>/5
+        [HttpGet("{username}")]
+        [Authorize]
+        public IEnumerable<Activity> Get(string username)
+        {
+            IStreamFeed userTimeline = _streamApi.StreamClient.Feed("timeline", username);
+            return userTimeline.GetFlatActivities().Result.Results;
+        }
+
+    }
+}
