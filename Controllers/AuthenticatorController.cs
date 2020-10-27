69b77a9d87fb6bd0348d9ab7bd2ee54129ef1d44
Joel <uo226321@uniovi.es>
2020-10-27 23:11:52 +0100

    Closes #2, closes #8 added controller to login users getting JWT tokens

---
+++ Controllers/AuthenticatorController.cs
@@ -0,0 +1,38 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RecYouBackend.Model;
+using RecYouBackend.Util;
+
+namespace RecYouBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticatorController : ControllerBase
+    {
+        private readonly IDatabaseConnection _database;
+
+        public AuthenticatorController(IDatabaseConnection database)
+        {
+            _database = database;
+
+        }
+
+        // POST api/<AuthenticatorController>        
+        [HttpPost]
+        public string Post([FromBody] UserDto userDto)
+        {
+            var pic = _database.GetInstance.QuerySingleOrDefault<string>("SELECT pic_url FROM users WHERE username = @user AND pass = @pass", new { user = userDto.Username, pass = userDto.Password });
+            if(pic != null)
+            {
+                return JWT.GenerateToken(userDto.Username);
+            } else
+            {
+                return "Invalid credentials";
+            }
+        }
+
+        
+
+    }
+}
