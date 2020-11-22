9f498b49009f9515befafcaa34c5c87969038cf0
joelalcala <UO226321@uniovi.es>
2020-11-22 23:57:07 +0100

    Added foreign id to the model

--- Model/PostDto.cs
+++ Model/PostDto.cs
@@ -5,6 +5,6 @@
         public string PictureUrl { get; set; }
         public string AudioUrl { get; set; }
         public string Description { get; set; }
-
+        public string ForeignId { get; set; }
     }
 }
